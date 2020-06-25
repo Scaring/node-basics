3bdf220bcaf03a7c6a561974d49100fcf33f7955
Dmytro Kuschevyy <scaring22@gmail.com>
2020-06-25 16:50:54 +0300

    add documentation to contacts methods

--- contacts.js
+++ contacts.js
@@ -4,17 +4,20 @@ const rootPath = __dirname;
 
 const contactsPath = path.join(rootPath, '/db', '/contacts.json');
 
+//Function returns array of objects from contacts.json
 function listContacts() {
   const list = fs.readFileSync(contactsPath, 'utf-8');
   return JSON.parse(list);
 }
 
+//Function returns object of exect contact searched by id in contacts.json
 function getContactById(contactId) {
   const contacts = listContacts();
   const searchedContact = contacts.find(contact => contact.id === contactId);
   return searchedContact;
 }
 
+//Function removes object of exect contact from contacts.json by id and return it
 function removeContact(contactId) {
   const contacts = listContacts();
   const deletedContact = getContactById(contactId);
@@ -23,6 +26,7 @@ function removeContact(contactId) {
   return deletedContact;
 }
 
+//Function add object of new contact to contacts.json and return it
 function addContact(name, email, phone) {
   const contacts = listContacts();
   const lastContact = contacts[contacts.length - 1];
