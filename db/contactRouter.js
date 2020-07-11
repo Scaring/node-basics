bb68ceb5059c007c33e252d7a52a4288e5eba961
scaringHome <scaring22@gmail.com>
2020-07-11 23:31:34 +0200

    finished router

---
+++ db/contactRouter.js
@@ -0,0 +1,92 @@
+const { Router } = require('express');
+const contacts = require('./contacts');
+
+const contactsRouter = Router();
+
+contactsRouter.get('/contacts/', async (req, res) => {
+  try {
+    const list = await contacts.listContacts();
+    res.status(200).send(list);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+contactsRouter.get('/contacts/:contactId', async (req, res) => {
+  try {
+    const searchedContactId = Number(req.params.contactId);
+    const searchedContact = await contacts.getContactById(searchedContactId);
+
+    if (!searchedContact) {
+      res.status(404).send({ message: 'Not found' });
+      return;
+    }
+
+    res.status(200).send(searchedContact);
+    console.log('Contact sent!');
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+contactsRouter.post('/contacts/', async (req, res) => {
+  try {
+    const { name, email, phone } = req.body;
+
+    if (!name || !email || !phone) {
+      res.status(404).send({ message: 'missing required name field' });
+      return;
+    }
+
+    const adddedContact = await contacts.addContact(name, email, phone);
+    res.status(201).send(adddedContact);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    res.end();
+  }
+});
+
+contactsRouter.delete('/contacts/:contactId', async (req, res) => {
+  try {
+    const searchedContactId = Number(req.params.contactId);
+    const removedContact = await contacts.removeContact(searchedContactId);
+
+    if (!removedContact) {
+      res.status(404).send({ message: 'Not found' });
+      return;
+    }
+
+    res.status(200).send({ message: 'contact deleted' });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+contactsRouter.patch('/contacts/:contactId', async (req, res) => {
+  try {
+    const searchedContactId = Number(req.params.contactId);
+    const updateData = req.body;
+
+    if (Object.keys(updateData).length === 0) {
+      res.status(404).send({ message: 'missing required name field' });
+      return;
+    }
+
+    const updatedContact = await contacts.updateContact(
+      searchedContactId,
+      updateData,
+    );
+
+    if (!updatedContact) {
+      res.status(404).send({ message: 'Not found' });
+      return;
+    }
+
+    res.status(200).send(updatedContact);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+module.exports = contactsRouter;
--- index.js
+++ index.js
@@ -1,64 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const contacts = require('./contacts');
+const contactsRouter = require('./db/contactRouter');
 
 const app = express();
 
-// app.use(cors);
-// app.use(express.urlencoded());
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-app.use('api/', async (req, res) => {
-  try {
-    const list = await contacts.listContacts();
-    res.status(200);
-    res.send(list);
-  } catch (e) {
-    console.log(e);
-  }
-});
+app.use('/', contactsRouter);
 
 app.listen(3000, () => console.log('Server is listening on port: 3000'));
-
-// const argv = require('yargs').argv;
-
-// async function invokeAction({ action, id, name, email, phone }) {
-//   switch (action) {
-//     case 'list':
-//       const list = await contacts.listContacts();
-//       console.log('Contacts list:');
-//       console.table(list);
-//       break;
-
-//     case 'get':
-//       const searchedContact = await contacts.getContactById(id);
-//       console.log('Searched contact:');
-//       console.log(searchedContact);
-//       break;
-
-//     case 'add':
-//       const addedContact = await contacts.addContact(name, email, phone);
-//       const listWithAddedContact = await contacts.listContacts();
-
-//       console.log('Added contact:');
-//       console.log(addedContact);
-//       console.log('Contacts list:');
-//       console.table(listWithAddedContact);
-//       break;
-
-//     case 'remove':
-//       const removedContact = await contacts.removeContact(id);
-//       const listWithoutRemovedContact = await contacts.listContacts();
-
-//       console.log('Deleted contact:');
-//       console.log(removedContact);
-//       console.log('Contacts list:');
-//       console.table(listWithoutRemovedContact);
-//       break;
-
-//     default:
-//       console.warn('\x1B[31m Unknown action type!');
-//   }
-// }
-
-// invokeAction(argv);
